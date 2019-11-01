def78b3268f7ce94d5c494da0c98e42b7318eb6d
Kerrie Yee <kerrie@thredup.com>
2019-11-01 09:24:34 -0700

    minor tweaks

--- app/services/generate_study_session_service.rb
+++ app/services/generate_study_session_service.rb
@@ -23,7 +23,7 @@ class GenerateStudySessionService
   private
 
   def create_details(ss)
-    #TODO allow configuration to choose how long session is
+    #TODO allow configuration to choose how long session is and update accordingly
     words = words_to_study_again.shuffle.first(5) + new_words_to_study.shuffle.first(5)
 
     query = "INSERT INTO `study_session_details` (`study_session_id`,`word_type`, `word_id`, `form`, `created_at`, `updated_at`)"
